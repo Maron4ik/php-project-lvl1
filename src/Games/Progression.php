01ffa7ffbc33f49e0878b37efe88d3c02f3d3aa5
Alexey Maron <maron.alexey@gmail.com>
2021-10-03 15:38:07 +0300

    prigression refactoring

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -4,30 +4,23 @@ namespace BrainGames\Games;
 
 use function BrainGames\runEngine;
 
-function generateProgression($firstNumber, $lastNumber, $numberStepProgression)
-{
-    $arithmeticProgression = [];
-    for ($i = $firstNumber; $i < $lastNumber; $i += $numberStepProgression) {
-        $arithmeticProgression[] = $i;
-    }
-
-    return $arithmeticProgression;
-}
-
 function runProgressionGame()
 {
     $getRightAnswerForRound = function () {
-        $randomLastNumber = 100;
-        $randomFirstNumber = rand(0, 2);
-        $randomStepProgression = rand(0, 3);
-        $arithmeticProgression = generateProgression($randomFirstNumber, $randomLastNumber, $randomStepProgression);
-        $skippedKey = array_rand($arithmeticProgression);
-        $rightAnswer = $arithmeticProgression[$skippedKey];
-        $arithmeticProgression[$skippedKey] = '..';
-        $roundQuestion = implode(' ', $arithmeticProgression);
+        $randomFirstNumber = rand(0, 10);
+        $randomStepProgression = rand(1, 5);
+        $randomArrLength = rand(5, 10);
+        $array = [];
 
+        for ($j = 0; $j < $randomArrLength; $j++) {
+            $randomFirstNumber += $randomStepProgression;
+            $array[$j] = $randomFirstNumber;
+        }
+        $skippedKey = rand(0, count($array) - 1);
+        $rightAnswer = $array[$skippedKey];
+        $array[$skippedKey] = '..';
+        $roundQuestion = implode(' ', $array);
         return ['roundQuestion' => $roundQuestion, 'rightAnswer' => $rightAnswer];
     };
-
     runEngine($getRightAnswerForRound, 'What number is missing in the progression?');
 }
