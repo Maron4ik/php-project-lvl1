e1750f388595297675abbdcdbdc208fdd3ea4e05
Alexey Maron <maron.alexey@gmail.com>
2021-10-04 22:02:34 +0300

    typehint

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -9,7 +9,7 @@ use function BrainGames\roundData;
 /**
  * @throws Exception
  */
-function generateComputedExpression(int $firstNumber, int $secondNumber, string $operation): float|int
+function generateComputedExpression(int $firstNumber, int $secondNumber, string $operation): int
 {
     switch ($operation) {
         case '+':
